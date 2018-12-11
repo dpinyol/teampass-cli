378c4b429a2e79cf1ac817262bdc6a2b50c5a964
Vadim Aleksandrov <valeksandrov@me.com>
2018-12-11 12:22:28 +0300

    Rename delete command to remove

--- tpcli/commands/cmd_remove.py
+++ tpcli/commands/cmd_remove.py
@@ -4,13 +4,13 @@ import click
 from tpcli.cli import pass_context
 
 
-@click.command('delete', short_help='delete entry from Teampass')
-@click.option('--item', 'type', flag_value='item', default=True, help='delete item')
-@click.option('--folder', 'type', flag_value='folder', help='delete folder with sub-folders and items')
+@click.command('remove', short_help='remove entry from Teampass')
+@click.option('--item', 'type', flag_value='item', default=True, help='remove item')
+@click.option('--folder', 'type', flag_value='folder', help='remove folder with sub-folders and items')
 @click.option('--id', required=True, help='entry id')
 @pass_context
 def cli(ctx, type, id):
-    """Delete entry from Teampass."""
+    """Remove entry from Teampass."""
     try:
         data = ctx.tp.delete(type, id)
     except Exception as ex:
